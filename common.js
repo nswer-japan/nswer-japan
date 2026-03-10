4a6b5dfca4e47d94ca85ebeef5aa8eb2164ab9e0
nswer-japan <satochantrumpet@gmail.com>
2026-03-10 23:53:15 +0900

    Add files via upload

--- common.js
+++ common.js
@@ -1,3 +1,20 @@
+// Google Analytics
+(function(){
+  const s1=document.createElement('script');
+  s1.async=true;
+  s1.src="https://www.googletagmanager.com/gtag/js?id=G-ZL16881EGB";
+  document.head.appendChild(s1);
+
+  const s2=document.createElement('script');
+  s2.innerHTML=`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-ZL16881EGB');
+  `;
+  document.head.appendChild(s2);
+})();
+
 document.addEventListener('DOMContentLoaded',()=>{
   const h=document.getElementById('hamburger');
   const m=document.getElementById('mobileMenu');
